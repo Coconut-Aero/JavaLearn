e854b9185e8615f55692a8086e083fd93ca3c9b3
Coconut-Aero <XiaoYi-Zero@office4xiaoyi.onmicrosoft.com>
2024-03-28 23:25:23 +0800

    New Class cityorder.java

--- README.md
+++ README.md
@@ -11,5 +11,6 @@ Updated 2024-03-27 11:14 CST
 4. trianglearea.java（输入三个点的坐标，输出它们构成的三角形的面积（海伦公式））
 5. daysinmonth.java（某年某月所包含的天数）
 6. srpgame.java（剪刀石头布游戏）
-7. srpwin2（人胜利两次自动终止循环的变种）
-8. numberpyramid（数字金字塔，2的幂）
+7. srpwin2.java（人胜利两次自动终止循环的变种）
+8. numberpyramid.java（数字金字塔，2的幂）
+9. cityorder.java（把城市按字母顺序排序）
---
+++ src/cityorder.java
@@ -0,0 +1,98 @@
+import java.util.*;
+public class cityorder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //Create a new scanner
+        System.out.print("Enter the first city ");
+        String city1= input.next();
+        System.out.println();
+        System.out.print("Enter the second city ");
+        String city2= input.next();
+        System.out.println();
+        System.out.print("Enter the third city ");
+        String city3= input.next();
+        System.out.println();
+
+        boolean allsame = false;
+        String Biggest = null;
+        String Smallest = null;
+        String Middle = null;
+        int compare1 = city1.compareTo(city2);
+        int compare2 = city2.compareTo(city3);
+        int compare3 = city1.compareTo(city3);
+        if (compare1==0&&compare2==0&&compare3==0){
+            Biggest = city3;
+            Middle = city2;
+            Smallest = city1;
+        }
+        else if (compare1==0) {
+            if(compare2>0){
+                Biggest = city1;
+                Middle = city2;
+                Smallest = city3;
+            }
+            else{
+                Biggest = city3;
+                Middle = city1;
+                Smallest = city2;
+            }
+        }
+        else if (compare2==0) {
+            if(compare3>0){
+                Biggest = city1;
+                Middle = city3;
+                Smallest = city2;
+            }
+            else{
+                Biggest = city2;
+                Middle = city3;
+                Smallest = city1;
+            }
+        }
+        else if (compare3==0) {
+            if(compare1>0){
+                Biggest = city1;
+                Middle = city3;
+                Smallest = city2;
+            }
+            else{
+                Biggest = city2;
+                Middle = city1;
+                Smallest = city3;
+            }
+        }
+        else if (compare1<0&&compare2<0){
+            Biggest = city3;
+            Middle = city2;
+            Smallest = city1;
+        }
+        else if (compare1>0&&compare2>0){
+            Biggest = city1;
+            Middle = city2;
+            Smallest = city3;
+        }
+        else if (compare1<0){
+            Biggest = city2;
+            if (compare3>0){
+                Middle = city1;
+                Smallest = city3;
+            }
+            else {
+                Middle = city3;
+                Smallest = city1;
+            }
+        }
+        else{
+            if (compare3>0){
+                Biggest = city1;
+                Middle = city3;
+            }
+            else {
+                Biggest = city3;
+                Middle = city1;
+            }
+            Smallest = city2;
+        }
+        System.out.println("The three cities in alphabetical order are "+ Smallest + " " + Middle + " " + Biggest);
+    }
+}
