cd7bf647f23a546fe5131b344323ee861601a508
Coconut-Aero <148566655+Coconut-Aero@users.noreply.github.com>
2024-05-02 11:43:08 +0800

    New File & README.md updated

--- README.md
+++ README.md
@@ -52,37 +52,38 @@ practice/BooleanWrapper.java 提供可以在方法内部修改并传递到方法
 
 包含：
 
-| 序号 | 文件名                              | 描述                           | 备注   |
-|----|----------------------------------|------------------------------|------|
-| 1  | homework/test.java               | Hello World                  | 作业   |
-| 2  | homework/time.java               | 输出系统的UNIX时间戳指向的日期（已经完善）      | 作业   |
-| 3  | homework/sumDigit.java           | 一个0~1000的数字各位的和              | 作业   |
-| 4  | homework/triangleArea.java       | 输入三个点的坐标，输出它们构成的三角形的面积（海伦公式） | 作业   |
-| 5  | homework/daysInMonth.java        | 某年某月所包含的天数                   | 作业   |
-| 6  | homework/SRPGame.java            | 剪刀石头布游戏                      | 作业   |
-| 7  | homework/SRPGameWin2.java        | 人胜利两次自动终止循环的变种               | 作业   |
-| 8  | practice/numberPyramid.java      | 数字金字塔，2的幂                    | 作业   |
-| 9  | practice/cityOrder.java          | 把城市按字母顺序排序                   | 4.24 |
-| 10 | practice/estimatePI.java         | 计算圆周率                        | 6.14 |
-| 11 | homework/arrayTest.java          | 输入一个数组，将其变换后输出               | 作业   |
-| 12 | homework/enhancedRandom.java     | 增强的random，可以选择范围，排除一些不想要的数字  | 作业   |
-| 13 | homework/intSum.java             | 命令行参数计算多个数字的和                | 作业   |
-| 14 | practice/hangman.java            | 猜单词的小游戏                      | 7.35 |
-| 15 | practice/quadSolve.java          | 解一元二次方程，方法可以调用               | 7.25 |
-| 16 | practice/sorted.java             | 检查数组是否按从小到大排序了               | 7.19 |
-| 17 | practice/lockerPuzzle.java       | 储物柜难题                        | 7.23 |
-| 18 | practice/mergeArray.java         | 合并两个有序列表                     | 7.31 |
-| 19 | practice/eightQueens.java        | 八皇后难题                        | 7.36 |
-| 20 | practice/repeatedInArray.java    | 计算数字的出现次数                    | 7.3  |
-| 21 | practice/galtonBoard.java        | 高尔顿瓶问题                       | 7.37 |
-| 22 | practice/numberSingleDigit.java  | 由100个个位数组成的数组中各个数字出现的次数统计    | 7.7  |
-| 23 | practice/sumColumnTest.java      | 读取一个3×4的矩阵求每列数字的和            | 8.1  |
-| 24 | practice/employeeWorkHours.java  | 每个雇员每周工作的小时数                 | 8.4  |
-| 25 | homework/addMatrixTest.java      | 两个矩阵相加                       | 作业   |
-| 26 | homework/maxRowCol.java          | 最大的行和列                       | 作业   |
-| 27 | practice/multiplyMatrixTest.java | 两个矩阵相乘                       | 8.6  |
-| 28 | homework/rectangleTest.java      | Rectangle类                   | 作业   |
-| 29 | practice/randomClassTest.java    | 使用Random类                    | 作业   |
+| 序号 | 文件名                              | 描述                           | 备注                      |
+|----|----------------------------------|------------------------------|-------------------------|
+| 1  | homework/test.java               | Hello World                  | 作业                      |
+| 2  | homework/time.java               | 输出系统的UNIX时间戳指向的日期（已经完善）      | 作业                      |
+| 3  | homework/sumDigit.java           | 一个0~1000的数字各位的和              | 作业                      |
+| 4  | homework/triangleArea.java       | 输入三个点的坐标，输出它们构成的三角形的面积（海伦公式） | 作业                      |
+| 5  | homework/daysInMonth.java        | 某年某月所包含的天数                   | 作业                      |
+| 6  | homework/SRPGame.java            | 剪刀石头布游戏                      | 作业                      |
+| 7  | homework/SRPGameWin2.java        | 人胜利两次自动终止循环的变种               | 作业                      |
+| 8  | practice/numberPyramid.java      | 数字金字塔，2的幂                    | 作业                      |
+| 9  | practice/cityOrder.java          | 把城市按字母顺序排序                   | 4.24                    |
+| 10 | practice/estimatePI.java         | 计算圆周率                        | 6.14                    |
+| 11 | homework/arrayTest.java          | 输入一个数组，将其变换后输出               | 作业                      |
+| 12 | homework/enhancedRandom.java     | 增强的random，可以选择范围，排除一些不想要的数字  | 作业                      |
+| 13 | homework/intSum.java             | 命令行参数计算多个数字的和                | 作业                      |
+| 14 | practice/hangman.java            | 猜单词的小游戏                      | 7.35                    |
+| 15 | practice/quadSolve.java          | 解一元二次方程，方法可以调用               | 7.25                    |
+| 16 | practice/sorted.java             | 检查数组是否按从小到大排序了               | 7.19                    |
+| 17 | practice/lockerPuzzle.java       | 储物柜难题                        | 7.23                    |
+| 18 | practice/mergeArray.java         | 合并两个有序列表                     | 7.31                    |
+| 19 | practice/eightQueens.java        | 八皇后难题                        | 7.36                    |
+| 20 | practice/repeatedInArray.java    | 计算数字的出现次数                    | 7.3                     |
+| 21 | practice/galtonBoard.java        | 高尔顿瓶问题                       | 7.37                    |
+| 22 | practice/numberSingleDigit.java  | 由100个个位数组成的数组中各个数字出现的次数统计    | 7.7                     |
+| 23 | practice/sumColumnTest.java      | 读取一个3×4的矩阵求每列数字的和            | 8.1                     |
+| 24 | practice/employeeWorkHours.java  | 每个雇员每周工作的小时数                 | 8.4                     |
+| 25 | homework/addMatrixTest.java      | 两个矩阵相加                       | 作业                      |
+| 26 | homework/maxRowCol.java          | 最大的行和列                       | 作业                      |
+| 27 | practice/multiplyMatrixTest.java | 两个矩阵相乘                       | 8.6                     |
+| 28 | homework/rectangleTest.java      | Rectangle类                   | 作业                      |
+| 29 | practice/randomClassTest.java    | 使用Random类                    | 作业                      |
+| 30 | practice/buckshotTest.java       | 俄罗斯轮盘赌游戏（目前处于测试阶段）| 灵感源自游戏Buckshot Roulette |
 
 跨Class引用函数关系如下：
 
@@ -97,6 +98,3 @@ practice/BooleanWrapper.java 提供可以在方法内部修改并传递到方法
 | 7  | homework/enhancedRandom.java | practice/galtonBoard.java       | getRandom()     |
 | 8  | homework/enhancedRandom.java | practice/numberSingleDigit.java | getRandom()     |
 | 9  | practice/galtonBoard.java    | homework/maxRowCol.java         | BooleanRandom() |
-
-
-# buckshotTest.java尚未完成编辑，目前先行上传。
--- src/practice/buckshotTest.java
+++ src/practice/buckshotTest.java
@@ -1,23 +1,179 @@
 package practice;
 
 import homework.enhancedRandom;
+
+import java.util.Objects;
 import java.util.Scanner;
 
 public class buckshotTest {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String Player1, Player2;
-        int Player1Health = 6, Player2Health = 6;
-        int[] prop = buckshot.createProp();
-        boolean Player1Win = false, Player2Win = false;
-        boolean exit = false;
-        boolean[] shellFree, shellType;
+        String Player1Name, Player2Name, useProp = "", activePlayer = "", shotWho;
+        int Player1Health = 6, Player2Health = 6, playWho;
+        int[] Player1Prop = buckshot.createProp(), Player2Prop = buckshot.createProp();
+        boolean Player1Win = false, Player2Win = false, shotType;
+        boolean exit = false, knifeMore = false, Player1NoMove = false, Player2NoMove = false;
+        boolean[] shellType;
         System.out.print("Enter Player 1 's name: ");
-        Player1 = input.nextLine();
+        Player1Name = input.nextLine();
         System.out.print("Enter Player 2 's name: ");
-        Player2 = input.nextLine();
+        Player2Name = input.nextLine();
         while(!exit){
-            shellType= buckshot.shell();
+            for(int i = 0; i < 3; i++){
+                while (Player1Health > 0 && Player2Health > 0){
+                    shellType = buckshot.shell();
+                    playWho = 1;
+                    for(int j = 0; j < shellType.length; j++){
+                        buckshot.PrintHealth(Player1Health,Player2Health,Player1Name,Player2Name);
+                        buckshot.printProp(Player1Prop , Player1Name);
+                        buckshot.printProp(Player2Prop , Player2Name);
+                        if (playWho == 1){
+                            while (!useProp.equals("no")){
+                                activePlayer = Player1Name;
+                                System.out.print("\033[H\033[2J");
+                                System.out.flush();
+                                buckshot.PrintHealth(Player1Health,Player2Health,Player1Name,Player2Name);
+                                buckshot.printProp(Player1Prop , Player1Name);
+                                buckshot.printProp(Player2Prop , Player2Name);
+                                System.out.println("It's " + activePlayer + "'s turn !");
+                                System.out.println("If you would not to use prop, you can just input \" no\"");
+                                System.out.println("Usage: cigarette/knife/drug/handcuff/magnify/adrenaline/phone/reverser/no");
+                                System.out.print( activePlayer + " , which prop would you like to use ? >: ");
+                                useProp = input.nextLine();
+                                switch (useProp){
+                                    case "cigarette":
+                                        if(Player1Health != 6) {
+                                            Player1Health++;
+                                        }
+                                    case "knife":
+                                        knifeMore = true;
+                                    case "drug":
+                                        boolean a = buckshot.drugEffect();
+                                        if(a){
+                                            Player1Health += 2;
+                                        }else{
+                                            Player1Health--;
+                                        }
+                                    case "handcuff":
+                                        Player2NoMove = true;
+                                    case "magnify":
+                                        buckshot.magnifyingGlass(shellType[j]);
+                                    case "adrenaline":
+                                        buckshot.adrenaline(Player2Prop,Player1Prop,Player1Name,Player2Name);
+                                    case "phone":
+                                        buckshot.SpecialPhone(shellType);
+                                    case "reverser":
+                                        buckshot.reverser(shellType , j);
+                                }
+                            }
+                            System.out.println("SHOT TIME. It's " + activePlayer + "'s turn !");
+                            System.out.println("You are going to shot who? (You/Opposite)");
+                            shotWho = input.nextLine();
+                            shotType=buckshot.shot(shellType[j]);
+                            if(shotType){
+                                System.out.println("REAL SHELL!");
+                                if (Player2NoMove){
+                                    playWho = 1;
+                                }
+                                else playWho = 2;
+                                if (Objects.equals(shotWho, "You")){
+                                    Player1Health--;
+                                }
+                                else if (Objects.equals(shotWho, "Opposite")){
+                                    Player2Health--;
+                                }
+                                Player2NoMove = false;
+                            }
+                            else {
+                                System.out.println("NO SHELL!");
+                                Player2NoMove = false;
+                            }
+                        }
+                        else if (playWho == 2){
+                            while (!useProp.equals("no")){
+                                activePlayer = Player2Name;
+                                System.out.print("\033[H\033[2J");
+                                System.out.flush();
+                                buckshot.PrintHealth(Player1Health,Player2Health,Player1Name,Player2Name);
+                                buckshot.printProp(Player1Prop , Player1Name);
+                                buckshot.printProp(Player2Prop , Player2Name);
+                                System.out.println("It's " + activePlayer + "'s turn !");
+                                System.out.println("If you would not to use prop, you can just input \" no\"");
+                                System.out.println("Usage: cigarette/knife/drug/handcuff/magnify/adrenaline/phone/reverser/no");
+                                System.out.print( activePlayer + " , which prop would you like to use ? >: ");
+                                useProp = input.nextLine();
+                                switch (useProp){
+                                    case "cigarette":
+                                        if(Player2Health != 6) {
+                                            Player2Health++;
+                                        }
+                                    case "knife":
+                                        knifeMore = true;
+                                    case "drug":
+                                        boolean a = buckshot.drugEffect();
+                                        if(a){
+                                            Player2Health += 2;
+                                        }else{
+                                            Player2Health--;
+                                        }
+                                    case "handcuff":
+                                        Player1NoMove = true;
+                                    case "magnify":
+                                        buckshot.magnifyingGlass(shellType[j]);
+                                    case "adrenaline":
+                                        buckshot.adrenaline(Player1Prop,Player2Prop,Player2Name,Player1Name);
+                                    case "phone":
+                                        buckshot.SpecialPhone(shellType);
+                                    case "reverser":
+                                        buckshot.reverser(shellType , j);
+                                }
+                            }
+                            System.out.println("SHOT TIME. It's " + activePlayer + "'s turn !");
+                            System.out.println("You are going to shot who? (You/Opposite)");
+                            shotWho = input.nextLine();
+                            shotType=buckshot.shot(shellType[j]);
+                            if(shotType){
+                                System.out.println("REAL SHELL!");
+                                if (!Player1NoMove){
+                                    playWho = 1;
+                                }
+                                if (Objects.equals(shotWho, "You")){
+                                    if (!knifeMore){
+                                        Player1Health--;
+                                    }
+                                    else Player1Health -= 2;
+                                }
+                                else if (Objects.equals(shotWho, "Opposite")){
+                                    if (!knifeMore){
+                                        Player2Health--;
+                                    }
+                                    else Player2Health -= 2;
+                                }
+                            }
+                            else {
+                                System.out.println("NO SHELL!");
+                            }
+                            Player2NoMove = false;
+                        }
+                        if (Player1Health == 0 || Player2Health == 0){
+                            break;
+                        }
+                    }
+                    if (Player1Health == 0 || Player2Health == 0){
+                        break;
+                    }
+                    buckshot.addProp(Player1Prop);
+                    buckshot.addProp(Player2Prop);
+                }
+                if (Player1Health == 0) Player2Win = true;
+                else if (Player2Health == 0) Player1Win = true;
+            }
+            if (Player1Win){
+                System.out.println(Player1Name + " has won!");
+            }
+            else if (Player2Win){
+                System.out.println(Player2Name + " has won!");
+            }
             exit = buckshot.exit();
         }
     }
@@ -32,61 +188,42 @@ class buckshot {
         return shellType;
     }
     public static int[] createProp (){
-        int[] prop = new int[8];
+        int[] prop = new int[9];
         for (int i = 0; i < 8; i++) {
             prop[i] = 0;
         }
         for (int i = 0; i < 4; i++) {
-            prop[i]=enhancedRandom.getRandom(0,8,0);
+            prop[i]=enhancedRandom.getRandom(0,9,0);
         }
         return prop;
     }
-    public static int[] addProp(int[] prop) {
+    public static void addProp(int[] prop) {
         for (int i = 0; i < prop.length; i++) {
             if (prop[i] == 0) {
                 prop[i] = enhancedRandom.getRandom(0, 8, 0);
             }
         }
-        return prop;
     }
     public static void printProp(int[] prop, String playerName) {
-        int[] propStatic = new int[8];
-        for (int i = 0; i < prop.length; i++) {
-            switch (prop[i]) {
-                case 0:
-                    continue;
-                case 1:
-                    propStatic[1]++;
-                case 2:
-                    propStatic[2]++;
-                case 3:
-                    propStatic[3]++;
-                case 4:
-                    propStatic[4]++;
-                case 5:
-                    propStatic[5]++;
-                case 6:
-                    propStatic[6]++;
-                case 7:
-                    propStatic[7]++;
-            }
-        }
+        int[] propStatic = propStatic(prop);
         for (int i = 1; i < propStatic.length; i++) {
             switch (propStatic[i]) {
                 case 1:
                     System.out.println("Player " + playerName + " has " + propStatic[i] + " cigarette(s).");
                 case 2:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " knife(ves).");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " knife(ves).");
                 case 3:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " drug(s).");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " drug(s).");
                 case 4:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " magnifying glass(es)");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " handcuff(s)");
                 case 5:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " adrenaline");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " magnifying glass(es)");
                 case 6:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " special phone");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " adrenaline");
                 case 7:
-                    System.out.println("Player" + playerName + " has " + propStatic[i] + " reverser");
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " special phone");
+                case 8:
+                    System.out.println("Player " + playerName + " has " + propStatic[i] + " reverser");
             }
         }
     }
@@ -97,27 +234,29 @@ class buckshot {
             false：代表副作用
          */
     }
-    public static boolean magnifyingGlass(boolean shell){
-        return (shell);
+    public static void magnifyingGlass(boolean shell){
+        if(shell){
+            System.out.println(" REAL SHELL");
+        }
+        else {
+            System.out.println(" NOT REAL SHELL");
+        }
     }
-    public static boolean SpecialPhone(boolean[] shell){
-        return (shell[enhancedRandom.getRandom(0, shell.length, 0,9)]);
+    public static void SpecialPhone(boolean[] shell){
+        int i = enhancedRandom.getRandom(0, shell.length, 0,9);
+        if (shell[i]){
+            System.out.println("Shell " + i + " is a real shell.");
+        }
+        else System.out.println("Shell " + i + " is not a real shell.");
     }
-    public static boolean reverser(boolean shell){
-        return (!shell);
+    public static void reverser(boolean[] shell, int i){
+        shell[i] = !shell[i];
     }
-    public static void PrintHealth(int Player1Health, int Player2Health, int Player1Name, int Player2Name){
+    public static void PrintHealth(int Player1Health, int Player2Health, String Player1Name, String Player2Name){
         System.out.println("Player1 Named: "+ Player1Name + "have health " + Player1Health);
         System.out.println("Player2 Named: "+ Player2Name + "have health " + Player2Health);
     }
-    public static boolean shellFree (boolean[] shellFree){
-        for (int i = 0; i < shellFree.length; i++) {
-            if (!shellFree[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
+
     public static boolean exit (){
         Scanner input = new Scanner(System.in);
         System.out.print("Play Again? (Y/N): ");
@@ -134,6 +273,103 @@ class buckshot {
             }
         }
     }
+    public static boolean shot (boolean shellType){
+        return shellType;
+    }
+    public static void adrenaline(int[] NegativePlayerProp, int[] PositivePlayerProp, String PositivePlayerName, String NegativePlayerName){
+        Scanner input = new Scanner(System.in);
+        System.out.println("You are gonna to take which? ");
+        System.out.println("Usage: cigarette/knife/drug/handcuff/magnify/adrenaline/phone/reverser");
+        String putin = input.nextLine();
+        boolean putSuccess = false;
+        int put = 0;
+        int[] propStatic = propStatic(NegativePlayerProp);
+        switch (putin) {
+            case "cigarette":
+                if (propStatic[1] != 0){
+                    put = 1;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have cigarette!");
+                }
+            case "knife":
+                if (propStatic[2] != 0){
+                    put = 2;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have knife!");
+                }
+            case "drug":
+                if (propStatic[3] != 0){
+                    put = 3;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have drug!");
+                }
+            case "handcuff":
+                if (propStatic[4] != 0){
+                    put = 4;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have handcuff!");
+                }
+            case "magnify":
+                if (propStatic[5] != 0){
+                    put = 5;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have magnify!");
+                }
+            case "adrenaline":
+                if (propStatic[6] != 0){
+                    put = 6;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have adrenaline!");
+                }
+            case "phone":
+                if (propStatic[7] != 0){
+                    put = 7;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have special phone!");
+                }
+            case "reverser":
+                if (propStatic[8] != 0){
+                    put = 8;
+                }else {
+                    System.out.println("Player " + NegativePlayerName + "doesn't have reverser!");
+                }
+        }
+        for (int i = 0; i < PositivePlayerProp.length; i++) {
+            if (PositivePlayerProp[i] == 0) {
+                PositivePlayerProp[i] = put;
+                putSuccess = true;
+            }
+        }
+        if (!putSuccess){
+            System.out.println("Player " + PositivePlayerName + " has no free space to put.");
+        }
+    }
+    public static int[] propStatic(int[] prop){
+        int[] propStatic = new int[prop.length];
+        for (int j : prop) {
+            switch (j) {
+                case 0:
+                    continue;
+                case 1:
+                    propStatic[1]++;
+                case 2:
+                    propStatic[2]++;
+                case 3:
+                    propStatic[3]++;
+                case 4:
+                    propStatic[4]++;
+                case 5:
+                    propStatic[5]++;
+                case 6:
+                    propStatic[6]++;
+                case 7:
+                    propStatic[7]++;
+                case 8:
+                    propStatic[8]++;
+            }
+        }
+        return propStatic;
+    }
 }
 
 
@@ -142,8 +378,9 @@ class buckshot {
         1：回血烟
         2：刀
         3：药物
-        4：放大镜
-        5：肾上腺素
-        6：特殊手机
-        7：逆转器
+        4：手铐
+        5：放大镜
+        6：肾上腺素
+        7：特殊手机
+        8：逆转器
  */
