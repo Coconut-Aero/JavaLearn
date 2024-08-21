c2f0a0c25980ede5ba359b72188b15c7bd0bdb82
Coconut-Aero <148566655+Coconut-Aero@users.noreply.github.com>
2024-08-22 00:49:13 +0800

    New File & README.md updated

--- src/practice/RandomPassword.java
+++ src/practice/RandomPassword.java
@@ -7,53 +7,64 @@ import java.util.Scanner;
 
 public class RandomPassword {
     // A Random Password Generator.
-    public static void main(String[] args) throws Exception {
+    static String[] Numbers = {"1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "0"};
+    static String[] Capitals = {"A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z"};
+    static String[] Lowercases =  {"a", "b", "c", "d", "e", "f", "g",
+            "h", "i" ,"j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"};
+    static String[] Symbols = {"!", "@", "#", "$", "%", "^", "&", "*",
+            "(", ")", "_", "+", "-", "=", "[", "]",
+            "{", "}", "|", ";", ":", "'", ",", ".",
+            "<", ">", "/", "?"};
+    static String[] AllSymbols = {
+            "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "0",
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g",
+            "h", "i" ,"j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z",
+            "!", "@", "#", "$", "%", "^", "&", "*",
+            "(", ")", "_", "+", "-", "=", "[", "]",
+            "{", "}", "|", ";", ":", "'", ",", ".",
+            "<", ">", "/", "?"};
+    static String[] NoSymbols = {
+            "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "0",
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q", "R", "S", "T", "U",
+            "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g",
+            "h", "i" ,"j", "k", "l", "m", "n",
+            "o", "p", "q", "r", "s", "t", "u",
+            "v", "w", "x", "y", "z"};
+
+
+    public static String[][] GenerateRandomPassword() throws Exception {
         Scanner sc = new Scanner(System.in);
-        String[] Numbers = {"1", "2", "3", "4", "5",
-                            "6", "7", "8", "9", "0"};
-        String[] Capitals = {"A", "B", "C", "D", "E", "F", "G",
-                             "H", "I", "J", "K", "L", "M", "N",
-                             "O", "P", "Q", "R", "S", "T", "U",
-                             "V", "W", "X", "Y", "Z"};
-        String[] Lowercases =  {"a", "b", "c", "d", "e", "f", "g",
-                                "h", "i" ,"j", "k", "l", "m", "n",
-                                "o", "p", "q", "r", "s", "t", "u",
-                                "v", "w", "x", "y", "z"};
-        String[] Symbols = {"!", "@", "#", "$", "%", "^", "&", "*",
-                            "(", ")", "_", "+", "-", "=", "[", "]",
-                            "{", "}", "|", ";", ":", "'", ",", ".",
-                            "<", ">", "/", "?"};
-        String[] AllSymbols = {
-                "1", "2", "3", "4", "5",
-                "6", "7", "8", "9", "0",
-                "A", "B", "C", "D", "E", "F", "G",
-                "H", "I", "J", "K", "L", "M", "N",
-                "O", "P", "Q", "R", "S", "T", "U",
-                "V", "W", "X", "Y", "Z",
-                "a", "b", "c", "d", "e", "f", "g",
-                "h", "i" ,"j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z",
-                "!", "@", "#", "$", "%", "^", "&", "*",
-                "(", ")", "_", "+", "-", "=", "[", "]",
-                "{", "}", "|", ";", ":", "'", ",", ".",
-                "<", ">", "/", "?"};
-        String[] NoSymbols = {
-                "1", "2", "3", "4", "5",
-                "6", "7", "8", "9", "0",
-                "A", "B", "C", "D", "E", "F", "G",
-                "H", "I", "J", "K", "L", "M", "N",
-                "O", "P", "Q", "R", "S", "T", "U",
-                "V", "W", "X", "Y", "Z",
-                "a", "b", "c", "d", "e", "f", "g",
-                "h", "i" ,"j", "k", "l", "m", "n",
-                "o", "p", "q", "r", "s", "t", "u",
-                "v", "w", "x", "y", "z"};
-        String[][] Password = new String[20][16];
+        String[][] Temp = new String[20][16];
         System.out.print("Enter the type of Password, 1 for 4*4, 2 for all random, 3 for all random but no symbols:");
         int PasswordType = sc.nextInt();
+        return GeneratePassword(PasswordType, Temp);
+    }
+
+    public static String[][] GenerateRandomPasswordWithSpecialFormat(int Quantity, int Length) throws Exception {
+        String[][] Temp = new String[Quantity][Length];
+        return GeneratePassword(2, Temp);
+    }
+
+    public static String[][] GeneratePassword(int PasswordType , String[][] Password) throws Exception {
         if (PasswordType == 1) {
-            for (int i = 0; i < 20; i++) {
+            for (int i = 0; i < Password.length; i++) {
                 for (int k = 0; k < 4; k++) {
                     Password[i][k] = Numbers[enhancedRandom.getRandom(0, Numbers.length)];
                     Password[i][k+4] = Capitals[enhancedRandom.getRandom(0, Capitals.length)];
@@ -63,15 +74,15 @@ public class RandomPassword {
             }
         }
         else if (PasswordType == 2) {
-            for (int i = 0; i < 20; i++) {
-                for (int k = 0; k < 16; k++) {
+            for (int i = 0; i < Password.length; i++) {
+                for (int k = 0; k < Password[i].length; k++) {
                     Password[i][k] = AllSymbols[enhancedRandom.getRandom(0, AllSymbols.length)];
                 }
             }
         }
         else if (PasswordType == 3) {
-            for (int i = 0; i < 20; i++) {
-                for (int k = 0; k < 16; k++) {
+            for (int i = 0; i < Password.length; i++) {
+                for (int k = 0; k < Password[i].length; k++) {
                     Password[i][k] = NoSymbols[enhancedRandom.getRandom(0, NoSymbols.length)];
                 }
             }
@@ -79,10 +90,10 @@ public class RandomPassword {
         else {
             throw new Exception("RandomPassword.InvalidTypeError");
         }
-        for (int i = 0; i < 20; i++) {
-            for (int j = 0; j < enhancedRandom.getRandom(10000, 16000); j++) {
-                int q = enhancedRandom.getRandom(0, 16);
-                int m = enhancedRandom.getRandom(0, 16);
+        for (int i = 0; i < Password.length; i++) {
+            for (int j = 0; j < enhancedRandom.getRandom(50,100); j++) {
+                int q = enhancedRandom.getRandom(0, Password[i].length);
+                int m = enhancedRandom.getRandom(0, Password[i].length);
                 if (m==q){
                     j = -1;
                     continue;
@@ -93,7 +104,7 @@ public class RandomPassword {
             }
             arrayTest.printStringArrayNoSpace(Password[i],Password[i].length);
         }
-        System.exit(0);
+        return Password;
     }
     //爱你
     //爱你
